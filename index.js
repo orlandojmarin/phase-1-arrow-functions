e9479fbd99f4235a8ecac74bc2df13187776286d
Orlando Marin <orlandojermainemarin@gmail.com>
2021-02-23 20:40:50 -0800

    completed assignment

---
+++ index.js
@@ -0,0 +1,9 @@
+
+// "divide" arrow function
+const divide = (num1 = 2000, num2 = 100) => num1 / num2;
+
+// "square" arrow function
+const square = num3 => num3*num3;
+
+// "add" arrow function
+const add = (num4, num5) => num4 + num5;
